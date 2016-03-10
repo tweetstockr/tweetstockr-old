932b61be22e9f284b8bc6aee2856ab2444010953
Bu Kinoshita <bukinoshita@gmail.com>
2016-03-10 13:49:20 -0500

    Ordinal fix & Tokens fix

--- public/scripts/controllers/headerController.js
+++ public/scripts/controllers/headerController.js
@@ -11,15 +11,23 @@
         function onSuccess(response) {
           $scope.username = response.user.twitter.displayName;
           $scope.joysticket = response.user.joysticket;
-          if(response.user.joysticket){
+
+          if(response.user.joysticket) {
             $scope.joyUser = response.user.joysticket.username;
-          }else{
+          } else {
             $scope.joyUser = false;
           }
+
           $scope.twitterUser = response.user.twitter.username;
           $scope.balance = response.balance;
           $scope.ranking = response.ranking;
-          $scope.tokens = response.user.tokens;
+
+          if(response.user.tokens == undefined) {
+            $scope.tokens = 0;
+          } else {
+            $scope.tokens = response.user.tokens;
+          }
+
           // These are not being used yet...
           $scope.profileImage = response.user.twitter.profile_image;
           $scope.profileImageThumb = response.user.twitter.profile_image_normal;
